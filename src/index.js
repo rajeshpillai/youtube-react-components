24b7f6751d17a30149835a58ade7e0d2dfbffc0e
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-02-21 10:11:37 +0530

    Feature: Drag and Drop

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AppDragDropDemo from './AppDragDropDemo';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppDragDropDemo />, document.getElementById('root'));
 registerServiceWorker();
